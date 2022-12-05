8ea3d25350942441d92c580fa1929c3ab811ddfe
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-12-05 09:02:50 +0100

    add annotation for validation for the lastName

--- MVCSpring/src/main/java/com/example/mvcspring/Customer.java
+++ MVCSpring/src/main/java/com/example/mvcspring/Customer.java
@@ -1,7 +1,12 @@
 package com.example.mvcspring;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Customer {
     private String firstName;
+    @NotNull(message="is required")
+    @Size(min=1)
     private String lastName;
 
     public String getFirstName() {
